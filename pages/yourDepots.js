73210a17544b7b973c7e2ab330611bd881439a97
mmackenzie <mmackenzie40@my.bcit.ca>
2024-04-20 13:59:43 -0700

    sixty percent commit

--- pages/yourDepots.js
+++ pages/yourDepots.js
@@ -3,20 +3,21 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import Link from 'next/link'
 import NavBar from "@/components/NavBar";
+import React from "react";
 
-export default function Depots() {
+export default function yourDepots() {
     return(
         <>
         <main>
-        <div class="logInSettingsDepots">
+        <div class="logInSettingsYourDepots">
         <Image src={'/MiaCards/LogIn.png'} width={100} height={38}/>
         <Image src={'/MiaCards/SettingsIcon.png'} width={40} height={38}/>
        </div>
-       <div class="searchBarDepots">
+       <div class="searchBarYourDepots">
           <Image src={'/MiaCards/SearchBar.png'} width={422} height={80}/>
         </div>
-        <div class="depotsCard">
-            <Image src={'/MiaCards/Asset 2.png'} width={422} height={630}/>
+        <div class="yourDepotsCard">
+            <Image src={'/MiaCards/Asset 5.png'} width={415} height={630}/>
         </div>
         <div>
             <NavBar/>
